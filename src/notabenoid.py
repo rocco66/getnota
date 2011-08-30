dd5402c2d3866fe6887053f8f33e6e4511f70f62
Maxim Mitroshin <rocco66max@gmail.com>
2011-08-30 12:25:02 +0400

    Get content by chapter

--- src/notabenoid.py
+++ src/notabenoid.py
@@ -5,28 +5,82 @@ Created on 26.08.2011
 '''
 
 from urllib import request
+from datetime import datetime
 import lxml.html
 
 
 class Notabenoid(object):
     '''
-    classdocs
+    Class for receiving book from notabenoid.com.
     '''
 
-    NB_URL = 'http://notabenoid.com/book/'
-    XPATH_BOOK_NAME = '/html/body/div/div[2]/div/div/div/h1'
+    NB_URL = 'http://notabenoid.com'
+    BOOK_INFO_ID = 'Info'
+    CHAP_LIST_ID = 'ChapList'
+    CONTENT_ID = 'content'
+    END_OF_CHAPTER = 'Переведено на сайте www.notabenoid.com'
 
-    def __init__(self, id):
+    def __init__(self, book_id):
         '''
         Constructor
+        id  - book id from notabenoid.com.
         '''
-        self.id = id
-        url = Notabenoid.NB_URL + id
+        self.id = book_id
+        url = Notabenoid.NB_URL + '/book/' + book_id
         self.main_page = request.urlopen(url).read().decode()
         self.main_page = lxml.html.document_fromstring(self.main_page)
+        info = self.main_page.get_element_by_id(Notabenoid.BOOK_INFO_ID)
+        self.book_name = info.find('h1').text
+        self.img_url = info.find('img').attrib['src']
         
         
     def get_book_name(self):
-        #сделать по id='Info'
-        return self.main_page.xpath(Notabenoid.XPATH_BOOK_NAME)[0].text
+        '''
+        Return book name.
+        '''
+        return self.book_name
+    
+    def get_book_img(self):
+        '''
+        Return book image url.
+        '''
+        return self.img_url
+    
+    def get_date(self):
+        '''
+        Return date of book receiving.
+        '''
+        return datetime.now()
+    
+    def chapters_links(self):
+        '''
+        Generator for chapter links.
+        '''
+        chap_table = self.main_page.get_element_by_id(Notabenoid.CHAP_LIST_ID)
+        for tr in chap_table:
+            res = tr.attrib.get('id')
+            if res:
+                td = tr[-1]
+                a = td[0]
+                yield a.attrib['href']
+             
+    def end_of_chapter(self):
+        return None     
+        
+    def content(self):
+        '''
+        Generator for lines from book.
+        '''
+        for cl in self.chapters_links():
+            url = Notabenoid.NB_URL + cl
+            page = lxml.html.parse(url).getroot()
+            form = page.forms[-1]
+            page = lxml.html.parse(lxml.html.submit_form(form)).getroot()
+            content = page.get_element_by_id(Notabenoid.CONTENT_ID)
+            for p in content:
+                if Notabenoid.END_OF_CHAPTER == p.text:
+                    yield self.end_of_chapter()
+                yield p.text
+
+        
         
--- tests/tests.py
+++ tests/tests.py
@@ -9,12 +9,40 @@ from notabenoid import Notabenoid
 
 class Test(unittest.TestCase):
 
-    def testGetBookNameByID(self):
+    def testGetBookInfoByID(self):
         nb = Notabenoid('19980')
         self.assertEqual(nb.get_book_name(), 
                          'A DANCE WITH DRAGONS / ТАНЕЦ С ДРАКОНАМИ',
                          'wrong book name receive')
-
+        self.assertEqual(nb.get_book_img(), 
+                         'http://notabenoid.com/i//book/1/19980-45199.jpg',
+                         'wrong book image url receive')
+        
+    def testGetChapter(self):
+        nb = Notabenoid('19980')
+        gen = nb.chapters_links()
+        self.assertEqual(gen.__next__(),
+                         '/book/19980/65224/ready',
+                         'wrong chapters link receive')
+        
+    def testGetFirstLine(self):
+        nb = Notabenoid('19980')
+        self.assertEqual(nb.content().__next__(), 
+                         'A DANCE WITH DRAGONS / ТАНЕЦ С ДРАКОНАМИ A CAVIL ON CHRONOLOGY',
+                         'wrong first book line')
+    
+    def testGetFirstChapter(self):
+        nb = Notabenoid('19980')
+        for line in nb.content():
+            if not line:
+                break
+            else:
+                last_line = line
+        self.assertEqual(last_line, 
+                         '—Джордж Р. Р. Мартин Апрель 2011',
+                         'wrong first chapter')
+        
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
