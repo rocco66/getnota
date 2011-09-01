9c0bd0d1a6877f53664d3870bae9669ff420f14c
Maxim Mitroshin <rocco66max@gmail.com>
2011-09-01 12:43:09 +0400

    FB2 created and verified by scheme.

--- src/fb2creator.py
+++ src/fb2creator.py
@@ -4,7 +4,7 @@ Created on 31.08.2011
 @author: max
 '''
 
-import lxml.etree
+from lxml import etree
 import lxml.html
 
 
@@ -19,8 +19,10 @@ class FB2Creator():
         '''
         Constructor
         '''
-        self.name = name
+        self.name = name + '.fb2'
         self.data = []
+        self.root = etree.Element('FictionBook')
+        self.root.set("xmlns", "http://www.gribuser.ru/xml/fictionbook/2.0") 
         
     def add_data(self, data):
         '''
@@ -32,47 +34,67 @@ class FB2Creator():
         '''
         Create fb2 file.
         '''
-        try:
-            self.file = open(self.name + '.fb2', mode='w')
-            self.__write_header()
-            for d in self.data:
-                self.__write_data(d)
-        finally:
-            self.file.close()
+        self.__create_header()
+        for d in self.data:
+                self.__create_data(d)
+        self.__write_etree()        
         return self.__isValid()
         
-    def __write_data(self, data):
+    def __write_etree(self):
         '''
-        Write text data from data to fb2 file.
+        Write xml to fb2 file.
         '''
-        pass
+        et = etree.ElementTree(self.root)
+        et.write(self.name,
+                 xml_declaration=True,
+                 encoding='utf-8')
+        return True
+         
+        
+    def __create_data(self, data):
+        '''
+        Create text data from data for fb2.
+        '''
+        body = etree.SubElement(self.root, 'body')
+        #может еще надо применить тэг p к содержимому?
+        title = etree.SubElement(body, 'title')
+        etree.SubElement(title, 'p').text = self.name.split('.')[0]
+        section = etree.SubElement(body, 'section')
+        for d in self.data:
+            s = etree.SubElement(section, 'section')
+            etree.SubElement(s, 'p').text = d
+        
              
-    def __write_header(self):
+    def __create_header(self):
         '''
-        Write header to fb2 file.
+        Create header for fb2.
         '''
-        pass
+        desc = etree.SubElement(self.root, 'description')
+        #title-info
+        ti = etree.SubElement(desc, 'title-info')
+        etree.SubElement(ti, 'genre').text = 'literature_books'
+        author = etree.SubElement(ti, 'author')
+        etree.SubElement(author, 'first-name').text = 'Максим'
+        etree.SubElement(author, 'last-name').text = 'Митрошин'
+        etree.SubElement(ti, 'book-title').text = 'Танцы с драконами'
+        etree.SubElement(ti, 'lang').text = 'ru'
+        #document-info
+        di = etree.SubElement(desc, 'document-info')
+        author = etree.SubElement(di, 'author')
+        etree.SubElement(author, 'nickname').text = 'rocco66'
+        etree.SubElement(di, 'date',
+                         value='2002-10-15').text = '15 ноября 2002г., 19:53'
+        etree.SubElement(di, 'id').text = 'SuperID'
+        etree.SubElement(di, 'version').text = '1.0'
+        #publish-info
+        pi = etree.SubElement(desc, 'publish-info') 
+        
     
     def __isValid(self):
         '''
         Retur True if created fb2 is valid.
         '''
-        """
-        f = StringIO('''\
-... <xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-... <xsd:element name="a" type="AType"/>
-... <xsd:complexType name="AType">
-...   <xsd:sequence>
-...     <xsd:element name="b" type="xsd:string" />
-...   </xsd:sequence>
-... </xsd:complexType>
-... </xsd:schema>
-... ''')
->>> xmlschema_doc = etree.parse(f)
->>> xmlschema = etree.XMLSchema(xmlschema_doc)
-
->>> valid = StringIO('<a><b></b></a>')
->>> doc = etree.parse(valid)
->>> xmlschema.validate(doc)
-True
-        """
+        xmlschema_doc = etree.parse(open(FB2Creator.scheme, mode='r'))
+        xmlschema = etree.XMLSchema(xmlschema_doc)
+        book = etree.parse(open(self.name, mode='r'))
+        return xmlschema.validate(book)
